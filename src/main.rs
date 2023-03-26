3d4fd077cb2380cc003f15935479e6a26c1cb245
kratos2377 <shobityadav23@gmail.com>
2023-03-26 16:19:27 +0530

    Code refactored and matching engine added

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,24 @@
-
-enum BidOrAsk {
-   Bid,
-   Ask, 
-}
-
-#[derive(Debug)]
-struct Price {
-    integral: u64,
-    fractional: u64,
-    scalar: u64,
-}
-
-impl Price {
-    fn new(price: f64) -> Price {
-        let scalar = 100000;
-        let integral = price as u64;
-        let fractional = ((price% 1.0) * scalar as f64) as u64;
-        Price {
-            scalar,integral,fractional
-        }
-    }
-}
-
-struct Limit {
-    price: Price,
-    order: Vec<Order>,
-}
-
-impl Limit {
-    fn new(price: 64) -> Limit {
-        Limit {
-            price: Price::new(price),
-            orders: Vec::new(),
-        }
-    }
-}
-
-struct Order {
-    size: f64,
-    bid_or_ask: BidOrAsk,
-}
-
-
-impl Order {
-    fn new(bid_or_ask: BidOrAsk , size: f64) -> Order {
-        Order{
-            bid_or_ask , size
-        }
-    }
-}
+mod matching_engine;
+use matching_engine::engine::{MatchingEngine, TradingPair};
+use matching_engine::orderbook::{BidOrAsk, Order, Orderbook};
 
 fn main() {
-   let price = Price::new(50.5);
-   println!("{:?}" , price)
+    let buy_order_from_alice = Order::new(BidOrAsk::Bid, 5.5);
+    let buy_order_from_bob = Order::new(BidOrAsk::Bid, 2.45);
+
+    let mut orderbook = Orderbook::new();
+    orderbook.add_order(4.4, buy_order_from_alice);
+    orderbook.add_order(4.4, buy_order_from_bob);
+
+    let sell_order = Order::new(BidOrAsk::Ask, 6.5);
+    orderbook.add_order(20.0, sell_order);
+
+    // println!("{:?}", orderbook);
+
+    let mut engine = MatchingEngine::new();
+    let pair = TradingPair::new("BTC".to_string(), "USD".to_string());
+    engine.add_new_market(pair.clone());
+
+    let buy_order = Order::new(BidOrAsk::Bid, 6.5);
+    engine.place_limit_order(pair, 10.000, buy_order).unwrap();
 }
